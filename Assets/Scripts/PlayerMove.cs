93bb8e74347575822dc69eee87f7442c9c888ad0
sdy362 <seodye04@gmail.com>
2021-04-25 00:22:15 +0900

    기능추가: 총을 쏠때 효과음이 재생되는 기능 추가

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -6,11 +6,20 @@ public class PlayerMove : MonoBehaviour
 {
     public float speed = 10f;
     public float rotationSpeed = 10f;
+    public float fireDelay = 0.1f;
     Rigidbody rigid;
+
+    private AudioSource audioSource;
+
+    private float lastFireTime = 0f;
+
+    [Header("Audio clips")]
+    public AudioClip fireSound;
 
     private void Awake()
     {
         rigid = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     void Update()
@@ -19,5 +28,15 @@ public class PlayerMove : MonoBehaviour
         float z = Input.GetAxis("Vertical");
         rigid.velocity = transform.forward.normalized * z * speed * Time.deltaTime;
         rigid.rotation = rigid.rotation * Quaternion.Euler(0, x * rotationSpeed * Time.deltaTime, 0);
+
+        if(Input.GetButtonDown("Fire1"))
+        {
+            if (Time.time - lastFireTime > fireDelay)
+            {
+                audioSource.clip = fireSound;
+                audioSource.Play();
+                lastFireTime = Time.time;
+            }
+        }
     }
 }
